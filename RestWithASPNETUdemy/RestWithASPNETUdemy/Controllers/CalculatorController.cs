71e123889db2badb5a64ded5af924f223ab4e726
Alexandre Flach <flachalexandret@gmail.com>
2019-07-29 23:11:36 -0300

    add calculator controller

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString())
+;            }
+
+            return BadRequest("invalid input");
+        }
+
+        private decimal ConvertToDecimal(string firstNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(firstNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            decimal value;
+            return decimal.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out value);
+        }
+    }
+}
